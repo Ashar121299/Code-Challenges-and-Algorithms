1292005d689b37bb0d07835c33743c78adcdc11b
Ashar121299 <asharalmomani@yahoo.com>
2022-10-27 19:47:30 +0300

    last

--- python/code_challenges/linkedlist/challenge01/test_challenge01.py
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -1,25 +1,9 @@
 # Write your test here
-from challenge01 import delete
+import pytest 
+from challenge01 import *
 
 
 def test_case1():
-    class Node:
-        def __init__(self,value):
-           self.value = value
-           self.next = None
-    class LinkedList:
-        def __init__(self):
-          self.head = None
-
-        def append(self, node):
-            if self.head is None:
-                self.head = node
-            else:
-                current = self.head
-                while current.next is not None:
-                    current = current.next
-                current.next = node
-    
     linkedList = LinkedList()
     node1 = Node(9)
     linkedList.append(node1)
@@ -29,98 +13,52 @@ def test_case1():
     linkedList.append(node3)
     node4 = Node(8)
     linkedList.append(node4)
-    assert delete(linkedList,node3)==[9, 1, 8]
-
-
+    delete(node3)
+    assert linkedList.print_All() ==[9, 1, 8]
 
 def test_case2():
-    class Node:
-        def __init__(self,value):
-           self.value = value
-           self.next = None
-    class LinkedList:
-        def __init__(self):
-          self.head = None
-
-        def append(self, node):
-            if self.head is None:
-                self.head = node
-            else:
-                current = self.head
-                while current.next is not None:
-                    current = current.next
-                current.next = node
-    
-    linkedList = LinkedList()
+    linkedList2 = LinkedList()
     node1 = Node(4)
-    linkedList.append(node1)
+    linkedList2.append(node1)
     node2 = Node(5)
-    linkedList.append(node2)
+    linkedList2.append(node2)
     node3 = Node(9)
-    linkedList.append(node3)
+    linkedList2.append(node3)
     node4 = Node(7)
-    linkedList.append(node4)
-    assert delete(linkedList,node3)==[4, 5, 7]
-
+    linkedList2.append(node4)
+    delete(node1)
+    assert linkedList2.print_All() ==[5, 9, 7]
 
 def test_case3():
-    class Node:
-        def __init__(self,value):
-           self.value = value
-           self.next = None
-    class LinkedList:
-        def __init__(self):
-          self.head = None
-
-        def append(self, node):
-            if self.head is None:
-                self.head = node
-            else:
-                current = self.head
-                while current.next is not None:
-                    current = current.next
-                current.next = node
-    
-    linkedList = LinkedList()
+    linkedList3 = LinkedList()
     node1 = Node(1)
-    linkedList.append(node1)
+    linkedList3.append(node1)
     node2 = Node(3)
-    linkedList.append(node2)
+    linkedList3.append(node2)
     node3 = Node(7)
-    linkedList.append(node3)
+    linkedList3.append(node3)
     node4 = Node(9)
-    linkedList.append(node4)
-    assert delete(linkedList,node2)==[1, 7, 9]
+    linkedList3.append(node4)
+    delete(node2)
+    assert linkedList3.print_All() ==[1, 7, 9]
 
 
 def test_case4():
-    class Node:
-        def __init__(self,value):
-           self.value = value
-           self.next = None
-    class LinkedList:
-        def __init__(self):
-          self.head = None
-
-        def append(self, node):
-            if self.head is None:
-                self.head = node
-            else:
-                current = self.head
-                while current.next is not None:
-                    current = current.next
-                current.next = node
-    
-    linkedList = LinkedList()
+    linkedList4 = LinkedList()
     node1 = Node(15)
-    linkedList.append(node1)
+    linkedList4.append(node1)
     node2 = Node(26)
-    linkedList.append(node2)
+    linkedList4.append(node2)
     node3 = Node(8)
-    linkedList.append(node3)
+    linkedList4.append(node3)
     node4 = Node(10)
-    linkedList.append(node4)
-    assert delete(linkedList,node2)==[15, 8, 10]
+    linkedList4.append(node4)
+    delete(node3)
+    assert linkedList4.print_All() ==[15, 26, 10]
+
+
+
+
 
 
 
